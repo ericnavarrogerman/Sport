8d58d52e48498c7e8790d512a1f7a726114bd839
Eric jose navarro german <enavarro.extreme@gmail.com>
2021-08-16 10:34:36 -0500

    icono y spiner

--- app/src/main/java/com/example/sport/ui/ListTeamFragment.kt
+++ app/src/main/java/com/example/sport/ui/ListTeamFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHost
@@ -33,6 +36,33 @@ class ListTeamFragment : Fragment() {
         binding= FragmentListTeamBinding.bind(inflater.inflate(R.layout.fragment_list_team,container,false))
 
 
+
+
+        context?.let { ArrayAdapter.createFromResource(it,R.array.leagues, android.R.layout.simple_spinner_item).also { 
+            adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinner.adapter=adapter
+            binding.spinner.onItemSelectedListener = object :OnItemSelectedListener{
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+
+                    val selected = parent?.getItemAtPosition(position).toString().split(" ").get(0)
+                    model.getTeamsForLeague(selected)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+            }
+
+        } }
+
+
         val adapter = AdapterTeams().apply {
             clic={  team ->
 
@@ -50,7 +80,7 @@ class ListTeamFragment : Fragment() {
 
         }
 
-        model.getTeamsForLeague("4335")
+
         model.teams.observe(viewLifecycleOwner,{
 
             adapter.setDataList(ArrayList(it))
